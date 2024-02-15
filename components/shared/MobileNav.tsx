8aafcb40e63f5b45dcbebeeff3505af2d63ab582
safayet <safayetdib@gmail.com>
2024-02-15 17:01:18 +0600

    added clerk auth and mongodb

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,41 +1,41 @@
-import Image from 'next/image';
-import NavItems from './NavItems';
+import Image from "next/image";
+import NavItems from "./NavItems";
 import {
-	Sheet,
-	SheetContent,
-	SheetDescription,
-	SheetHeader,
-	SheetTitle,
-	SheetTrigger,
-} from '@/components/ui/sheet';
-import { Separator } from '@/components/ui/separator';
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Separator } from "@/components/ui/separator";
 
 const MobileNav = () => {
-	return (
-		<nav className='md:hidden'>
-			<Sheet>
-				<SheetTrigger className='align-middle'>
-					<Image
-						src='/assets/icons/menu.svg'
-						alt='Menu'
-						width={24}
-						height={24}
-						className='cursor-pointer'
-					/>
-				</SheetTrigger>
-				<SheetContent className='flex flex-col gap-6 bg-white md:hidden'>
-					<Image
-						src='/assets/images/logo.svg'
-						width={128}
-						height={38}
-						alt='Evently Logo'
-					/>
-					<Separator className='border border-gray-50' />
-					<NavItems />
-				</SheetContent>
-			</Sheet>
-		</nav>
-	);
+  return (
+    <nav className="md:hidden">
+      <Sheet>
+        <SheetTrigger className="align-middle">
+          <Image
+            src="/assets/icons/menu.svg"
+            alt="Menu"
+            width={24}
+            height={24}
+            className="cursor-pointer"
+          />
+        </SheetTrigger>
+        <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="Evently Logo"
+          />
+          <Separator className="border border-gray-50" />
+          <NavItems />
+        </SheetContent>
+      </Sheet>
+    </nav>
+  );
 };
 
 export default MobileNav;
